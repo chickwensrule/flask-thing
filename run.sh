0f1ef17768da5eba4d3dd1c1133e2c987b88fc07
ChickwensRule <80529810+chickwensrule@users.noreply.github.com>
2024-03-05 14:17:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #ffmpeg -f v4l2 -i /dev/video0 -vcodec libx264 -preset ultrafast -tune zerolatency -an -f mpegts udp://0.0.0.0:8080
 
-ffmpeg -s 1024x786 -i /dev/video0 -preset ultrafast -tune zerolatency -codec libx264 -f mpegts udp://192.168.2.61:8090
+ffmpeg -s 1024x786 -i /dev/video0 -preset ultrafast -tune zerolatency -codec libx264 -f mpegts udp://192.168.1.2:8080
